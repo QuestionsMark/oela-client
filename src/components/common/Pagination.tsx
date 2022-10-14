205deefff546bf2151a2a9725386fbbe3e0c53e9
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-10-14 16:40:36 +0200

    pagination updated

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,3 +1,5 @@
+import { faAngleLeft, faAngleRight, faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Dispatch, SetStateAction } from "react";
 
 interface Props {
@@ -8,6 +10,7 @@ interface Props {
 }
 
 export const Pagination = ({ amount, limit, page, setPage }: Props) => {
+    const maxPage = Math.ceil(amount / limit);
     const handlePageChange = (page: number) => {
         if (!page) return;
         setPage(page);
@@ -15,9 +18,21 @@ export const Pagination = ({ amount, limit, page, setPage }: Props) => {
 
     return (
         <div className="pagination">
+            {page !== 1 && <div className="pagination__btn pagination__btn--large" onClick={() => handlePageChange(page - 1)}><FontAwesomeIcon icon={faAngleLeft} /></div>}
+
+            {page >= 4 && <div className="pagination__btn" onClick={() => handlePageChange(1)}>{1}</div>}
+            {![1,2,3,4].includes(page) && <div className="pagination__btn"><FontAwesomeIcon icon={faEllipsis} /></div>}
+            {page !== 1 && page !== 2 && <div className="pagination__btn pagination__btn--large" onClick={() => handlePageChange(page - 2)}>{page - 2}</div>}
             {page !== 1 && <div className="pagination__btn" onClick={() => handlePageChange(page - 1)}>{page - 1}</div>}
+
             <div className="pagination__btn pagination__btn--current">{page}</div>
-            {page * limit < amount && <div className="pagination__btn" onClick={() => handlePageChange(page + 1)}>{page + 1}</div>}
+
+            {page < maxPage && <div className="pagination__btn" onClick={() => handlePageChange(page + 1)}>{page + 1}</div>}
+            {page < maxPage - 1 && <div className="pagination__btn pagination__btn--large" onClick={() => handlePageChange(page + 2)}>{page + 2}</div>}
+            {page < maxPage - 3 && <div className="pagination__btn"><FontAwesomeIcon icon={faEllipsis} /></div>}
+            {page < maxPage - 2 && <div className="pagination__btn" onClick={() => handlePageChange(maxPage)}>{maxPage}</div>}
+
+            {page !== Math.ceil(amount / limit) && <div className="pagination__btn pagination__btn--large" onClick={() => handlePageChange(page + 1)}><FontAwesomeIcon icon={faAngleRight} /></div>}
         </div>
     );
 };
